590860b8eef24b601ac0fd5048c8e33b58104873
Rama Dondapati <rdondapati@gmail.com>
2022-11-15 22:09:08 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
+        <img src={logo} className="App-logo" alt="logo" />       
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React JS
         </a>
       </header>
     </div>
